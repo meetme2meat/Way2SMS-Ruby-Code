3c01cc5fedfae24c66e50640e701e151535bcc3e
Viren Negi <meetme2meat@gmail.com>
2018-07-19 15:53:26 +0530

    Change the default msg

--- way2sms.rb
+++ way2sms.rb
@@ -8,7 +8,7 @@ require 'cgi'
 URL = 'http://site1.way2sms.com'
 username = '' ## way2sms login
 password = '' ## way2sms password
-message = "Hello I'm Cheating Way2SMS :)" ## Message to be Sent
+message = "Hello World :)" ## Message to be Sent
 number = ''   # Receiver mobile number
 
 ## With the above change you need to provide your Action on line number
@@ -17,39 +17,39 @@ URL_REGEX = /http:\/\/site1.way2sms.com\/(.+)/
 
 def set_header(cookie=nil,referer=nil)
   {"Cookie" => cookie , "Referer" => referer ,"Content-Type" => "application/x-www-form-urlencoded","User-Agent" => "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20091020 Ubuntu/9.10 (karmic) Firefox/3.5.3 GTB7.0" }
-end 
+end
 
 http = Net::HTTP.new(uri.host,uri.port)
- 
+
 if uri.scheme == "https"
    http.use_ssl = true
    http.ca_path = "/etc/ssl/certs"
    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-end   
+end
 
 
 
 
-def fetch(http,path,header,data=nil,method=:get,limit=10) 
+def fetch(http,path,header,data=nil,method=:get,limit=10)
  raise ArgumentError, 'HTTP redirect too deep' if limit == 0
   cookie ||= header['Cookie']
   referer ||= header['Referer']
   if method == :get
     response = http.get(path,header.delete_if {|i,j| j.nil? })
-  else   
+  else
     response = http.post(path,data,header.delete_if {|i,j| j.nil? })
     cookie ||= response['set-cookie']
-  end 
-    
-  case response.code 
+  end
+
+  case response.code
     when   /2\d{2}/
-      return [cookie,referer,response,http]         
+      return [cookie,referer,response,http]
     when  /3\d{2}/
       cookie,referer,response,http = fetch(http,("/"+response['location'].match(URL_REGEX)[1]),set_header(cookie,(URL+path)),limit-1)
       return [cookie,referer,response,http]
    else
      return "HTTP Error"
-   end  
+   end
 end
 
 
@@ -65,9 +65,9 @@ cookie,referer,response,http = fetch(http,'/Login1.action',set_header(cookie,ref
 
 message = CGI.escape(message)
 
-## The Action obtained from the way2sms on page after successful login  the form of the quick message submission by inespecting it using firebuglite 
-## Action need to be replace with yours -------- 
-#                                              |  
+## The Action obtained from the way2sms on page after successful login  the form of the quick message submission by inespecting it using firebuglite
+## Action need to be replace with yours --------
+#                                              |
 sms_data = 'HiddenAction=instantsms&Action=hgfgh5656fgd&MobNo='+number+'&textArea='+message
 
 cookie,referer,response,http = fetch(http,'/quicksms.action',set_header(cookie,referer),sms_data,:post)
